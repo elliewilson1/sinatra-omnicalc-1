93c5d5f6bdb87841eee7a63f63ec87b8362b64da
elliewilson1 <ewilson0@chicagobooth.edu>
2025-04-15 02:02:03 +0000

    finished!

--- app.rb
+++ app.rb
@@ -10,9 +10,9 @@ get("/square/new") do
 end
 
 get("/square/results") do
-  @the_num = params.fetch("user_number").to_f
+  @the_num = params.fetch("user_number")
 
-  @the_result = @the_num ** 2
+  @the_result = @the_num.to_f ** 2
 
   erb(:square_results)
 end
@@ -22,9 +22,9 @@ get("/square_root/new") do
 end
 
 get("/square_root/results") do
-  @the_num = params.fetch("user_number").to_f
+  @the_num = params.fetch("user_number")
 
-  @the_result = @the_num ** 0.5
+  @the_result = @the_num.to_f ** 0.5
 
   erb(:square_root_results)
 end
@@ -34,6 +34,12 @@ get("/random/new") do
 end
 
 get("/random/results") do
+  @the_min = params.fetch("user_min").to_f
+
+  @the_max = params.fetch("user_max").to_f
+
+  @the_result = rand(@the_min..@the_max)
+
   erb(:random_results)
 end
 
@@ -42,5 +48,27 @@ get("/payment/new") do
 end
 
 get("/payment/results") do
+  @APR = params.fetch("APR").to_f
+
+  @user_APR = @APR.to_fs(:percentage, {:precision => 4})
+  
+  @rate = @APR / 1200
+
+  @years = params.fetch("years").to_i
+
+  @periods = @years * 12
+
+  @principal = params.fetch("principal").to_f
+
+  @user_principal = @principal.to_fs(:currency)
+
+  @denominator = 1 - (1 + @rate) ** -@periods
+
+  @numerator = @rate * @principal
+
+  @payment = @numerator / @denominator
+
+  @the_result = @payment.to_fs(:currency)
+
   erb(:payment_results)
 end
